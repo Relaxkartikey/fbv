e7cafa799671bdd532bdfe451ea6abb74d2cfc26
relaxkartikey <IKARTIKEYSHARMA@GMAIL.COM>
2025-02-25 13:23:07 +0530

    ok

--- pdflit/src/app/layout.tsx
+++ pdflit/src/app/layout.tsx
@@ -19,27 +19,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={inter.className}>
-      <body className="flex min-h-screen flex-col bg-background">
-        <Header />
-        <main className="flex-1 bg-gray-50/50">
-          {children}
-        </main>
-        <Footer />
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <link rel="stylesheet" href="/css/dflip.min.css" />
+        <link rel="stylesheet" href="/css/themify-icons.min.css" />
+      </head>
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <div className="relative flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-1 bg-gray-50/50">
+            {children}
+          </main>
+          <Footer />
+        </div>
         <DFlipScripts />
-        <Script id="load-dflip-css" strategy="beforeInteractive">
-          {`
-            const linkDflip = document.createElement('link');
-            linkDflip.rel = 'stylesheet';
-            linkDflip.href = '/css/dflip.min.css';
-            document.head.appendChild(linkDflip);
-
-            const linkThemify = document.createElement('link');
-            linkThemify.rel = 'stylesheet';
-            linkThemify.href = '/css/themify-icons.min.css';
-            document.head.appendChild(linkThemify);
-          `}
-        </Script>
       </body>
     </html>
   );
